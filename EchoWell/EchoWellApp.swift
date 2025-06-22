701056ce99f9fb558596a19833ca2eaa6a281c03
iameli <mateigrig85@gmail.com>
2025-06-22 10:43:36 +0300

    Initial Commit

---
+++ EchoWell/EchoWellApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoWellApp.swift
+//  EchoWell
+//
+//  Created by Matei Grigore on 6/22/25.
+//
+
+import SwiftUI
+
+@main
+struct EchoWellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
